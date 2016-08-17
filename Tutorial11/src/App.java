f5d4ab1cd8b060cd8c8bd0b042160ec74355c707
alexkurochka <alexander.kurochka@gmail.com>
2016-08-17 23:38:14 +0300

    first commit

---
+++ Tutorial11/src/App.java
@@ -0,0 +1,19 @@
+
+public class App {
+	public static void main(String[] args) {
+		String[] texts = new String[3];
+		
+		texts[0] = "Hello";
+		texts[1] = "to";
+		texts[2] = "you.";
+		System.out.println(texts[2]);
+		
+		String[] fruits = {"apple","kiwi","cherry"};
+		for(String fruit: fruits){
+			System.out.println(fruit);
+			
+		String text = null;
+		System.out.println(text);
+		}
+	}
+}
